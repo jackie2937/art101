0e1c01ec2e7efce5066219e350ce61d6716507c4
Jackie <114437004+jackie2937@users.noreply.github.com>
2022-10-17 10:37:47 -0700

    lab 5
    lab 5 pink prius

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary. make connect 4.
+// Description. make a two player game of connect 4.
+// @link   URL
+// @file   This files defines the gameplay loop for connect 4
+// @author Aidan Sterling, Jacqueline Wong .
+// @since  1.0.0
+
+// Create board
+	// Create piece for current player
+		// Let player pick move
+		// Pieces should fall to bottom of board
+		// Check for win condition
+		// Move to other player
+		// Repeat 2-6 until winner pieces connect 4
---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Aidan Sterling
+ * Author: Jacqueline Wong
+ * Created: 17 October
+ * Licence: Public Domain
+*/
+
+// define variables
+var make = "prius";
+var model = "insight";
+var year = 2022;
+var color = "pink";
+
+// calculate
+var age = 2022 - year;
+var ownIt = true;
+
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Ownit: " + ownIt + "<br>");
