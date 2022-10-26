4af39f90c2d27d049a26d111438f12813f27956b
Jackie <114437004+jackie2937@users.noreply.github.com>
2022-10-26 10:25:35 -0700

    sahevf
    weknqk

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -29,27 +29,48 @@ console.log(arrayNum.map(Multiplicator));
 console.log(arrayNum.map(function(x){
     return x/83;
 }));
-
+//add results to a string
 var results = "";
 //run function on 2
 results += "2 * (2): " + Multiplicator(2) + "<br>";
 //4
 
 //run function of 7324
-results += "2 * (2): " + Multiplicator(7324) + "<br>";
+results += "2 * (7324): " + Multiplicator(7324) + "<br>";
 //14648
 
 //map function to arrayNum
-results += "2 * (2): " + arrayNum.map(Multiplicator) + "<br>";
+results += "map(x*2) to arrayNum: " + arrayNum.map(Multiplicator) + "<br>";
 //(4) [18364, 476, -46, 6]0: 183641: 4762: -463: 6length: 4[[Prototype]]: Array(0)
 
 
 //run map on anon function
-results += "2 * (2): " + arrayNum.map(function(x){
+results += "map(x/83) to arrayNum: " + arrayNum.map(function(x){
     return x/83;
 }) + "<br>";
 
+//add function
+function add(x, y){
+  return x + y;
+};
+
+//mult function
+function mult(x, y){
+  return x*y;
+};
+
+//calculate using a callback
+function calculate(func, x, y){
+  return func(x, y);
+};
+
+//add results
+results += "add 2 and 3: " + calculate(add, 2, 3) + "<br>";
+results += "multiply 4 and 6: " + calculate(mult, 4, 6) + "<br>";
+
 //(4) [110.62650602409639, 2.8674698795180724, -0.27710843373493976, 0.03614457831325301]
 
+//set div with id output to results
 var outputEl = document.getElementById("output");
+console.log(outputEl);
 outputEl.innerHTML = results; // put your results here
