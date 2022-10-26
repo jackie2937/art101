409bd4d5467d9f4cb8870504ea06e123fcd54d86
Jackie <114437004+jackie2937@users.noreply.github.com>
2022-10-26 10:02:14 -0700

    push
    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -3,26 +3,29 @@
  * created wed. 26th
  * License: Public Domain
 */
-//
+
+//create array
 var arrayNum = [9182, 238, -23, 3];
-//
+//create function multiplicator
 function Multiplicator(x){
     var results = (x*2);
     return results;
 }
 
+//run function on 2
 console.log(Multiplicator(2));
 //4
-console.log(Multiplicator(2));
-//4
+
+//run function of 7324
 console.log(Multiplicator(7324));
 //14648
 
+//map function to arrayNum
 console.log(arrayNum.map(Multiplicator));
 //(4) [18364, 476, -46, 6]0: 183641: 4762: -463: 6length: 4[[Prototype]]: Array(0)
 
 
-
+//run map on anon function
 console.log(arrayNum.map(function(x){
     return x/83;
 }))
