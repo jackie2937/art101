cdecc5cfddef6f96b592b0c2d0c8e8a0418d56f2
Jackie <114437004+jackie2937@users.noreply.github.com>
2022-11-07 10:44:22 -0800

    lab11
    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,5 +1,28 @@
 /*
  * Author: Jackie and Aidan
- * created mon. 24th
+ * created wed. Nov 7th
  * License: Public Domain
 */
+var resultsButton = $("<button>");
+resultsButton.html("results");
+$("#results").append("<br>");
+$("#results").append(resultsButton);
+resultsButton.click(function(){
+  resultsButton.parent().toggleClass("special");
+});
+
+var challengesButton = $("<button>");
+challengesButton.html("challenges");
+$("#challenges").append("<br>");
+$("#challenges").append(challengesButton);
+challengesButton.click(function(){
+  challengesButton.parent().toggleClass("special");
+});
+
+var problemsButton = $("<button>");
+problemsButton.html("problems");
+$("#problems").append("<br>");
+$("#problems").append(problemsButton);
+problemsButton.click(function(){
+  problemsButton.parent().toggleClass("special");
+});
