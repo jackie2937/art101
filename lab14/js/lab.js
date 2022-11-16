eb9c27eb2ad5f4dc8dfeff403aad570dbb2e6eb9
Jackie <114437004+jackie2937@users.noreply.github.com>
2022-11-16 11:49:27 -0800

    lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,18 @@
+/*
+ * Author: Jackie and Aidan
+ * created wed. Nov 14th
+ * License: Public Domain
+*/
+/*
+ * Author: Jackie and Aidan
+ * created wed. Nov 14th
+ * License: Public Domain
+*/
+var a = 2;
+var b = 3;
+debugger;
+var x = a + b;
+var y = x + a - b;
+if(y === 2*a){
+  console.log("hello world!");
+}
