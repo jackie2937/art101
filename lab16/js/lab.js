77246df63f9d5f48f8566335708fdd3e7b3c39d1
Jackie <114437004+jackie2937@users.noreply.github.com>
2022-11-28 11:33:59 -0800

    lab 16 arkdjhgv

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,57 +1,75 @@
 /*
  * Author: Jackie and Aidan
- * created wed. Nov 21th
+ * created wed. Nov 28th
  * License: Public Domain
 */
+var comic = -1;
+var Data;
+var Start = true;
+var max = 100;
+function GetData(start){
+     if(start){
+          $.ajax({
+              // The URL for the request (from the api docs)
+              url: "https://xkcd.com/info.0.json",
+              // Whether this is a POST or GET request
+              type: "GET",
+              // The type of data we expect back
+              dataType : "json",
+              // What do we do when the api call is successful
+              //   all the action goes in here
+              success: function(data) {
+                  Data = data;
+                  console.log(data);
+                  Start = false;
+                  comic = data.num;
+                  max = data.num;
+                  $("#pic").attr("src", Data.img);
+              },
+              // What we do if the api call fails
+              error: function (jqXHR, textStatus, errorThrown) {
+                  // do stuff
+                  console.log("Error:", textStatus, errorThrown);
+              }
+         });
 
-var info = null;
-var dificulty = 2;
-function getData() {
-     $.ajax({
-          url: "https://jservice.io/api/random",
-          data: {
-               count: dificulty
-          },
-          type: "GET",
-          dataType: "json",
-          success: function(data) {
-               info = data;
-               //console.log(data);
-               var out = $("#output");
-               var qnum = Math.floor(Math.random()*dificulty);
-               console.log(qnum);
-               console.log(data[qnum]);
-               var temptxt = "question: " + data[qnum].question + "";
-               for(var i = 0; i < data.length;i++){
-                    temptxt += "<br><button id=qustion_" + i + ">" + data[i].answer + "</button>"
-               }
-               out.html(temptxt);
-
-
-               for(var i = 0; i < data.length;i++){
-                    if(i === qnum){
-                         $("#qustion_" + i).click(function(){
-                              $("#output").html("question: " + data[qnum].question + "<br>" + "Correct! good job<br>dificulty: " + dificulty);
-                              dificulty += 1;
-                         });
-                    } else{
-                         $("#qustion_" + i).click(function(){
-                              dificulty = 2;
-                              $("#output").html("question: " + data[qnum].question + "<br>" + "Sorry, the answer was " + data[qnum].answer + ", please try again.<br>dificulty: " + dificulty);
-                         });
-                    }
-               }
-               //var temp = "question: " + data[0].question + "<br><br>answer: " + data[0].answer;
-               //out.html(temp);
-          },
-          error: function (jqXHR, textStatus, errorThrown) {
-              info = null;
-              console.log("error");
-              $("#output").html("ERROR");
-          }
-     });
+    } else {
+         $.ajax({
+            // The URL for the request (from the api docs)
+            url: "https://xkcd.com/" + comic + "/info.0.json",
+            // Whether this is a POST or GET request
+            type: "GET",
+            // The type of data we expect back
+            dataType : "json",
+            // What do we do when the api call is successful
+            //   all the action goes in here
+            success: function(data) {
+                 Data = data;
+                 console.log(data);
+                 comic = data.num;
+                 $("#pic").attr("src", data.img);
+            },
+            // What we do if the api call fails
+            error: function (jqXHR, textStatus, errorThrown) {
+                 // do stuff
+                 console.log("Error:", textStatus, errorThrown);
+            }
+        });
+    }
 }
 
-var button=$("#activate");
-button.click(getData);
-getData();
+GetData(Start);
+$("#last").click(function(){
+     comic -= 1;
+     if(comic < 0){
+          comic = 0;
+     }
+     GetData(Start);
+});
+$("#next").click(function(){
+     comic += 1;
+     if(comic > max){
+          comic = max;
+     }
+     GetData(Start);
+});
